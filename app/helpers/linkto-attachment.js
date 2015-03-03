150aee12432a26135d12acb3590f8153e0281c76
Sam Heuck <sam@samheuck.com>
2015-03-02 23:08:00 -0600

    fixes references to environment vars
    Signed-off-by: Sam Heuck <sam@samheuck.com>

--- app/helpers/linkto-attachment.js
+++ app/helpers/linkto-attachment.js
@@ -1,8 +1,9 @@
+import config from '../config/environment';
 import Ember from 'ember';
 
 export default Ember.Handlebars.makeBoundHelper(function (attachment) {
     var html = "<a href='%@' target='_blank'>%@</a>",
-        url = "%@/%@/%@".fmt(LooseleafENV.dbHost, LooseleafENV.dbName, attachment.get('id'));
+        url = "%@/%@/%@".fmt(config.dbHost, config.dbName, attachment.get('id'));
 
     return new Ember.Handlebars.SafeString(html.fmt(url, attachment.get('file_name')));
 });
