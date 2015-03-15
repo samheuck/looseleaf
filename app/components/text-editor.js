ca6c1a1abfbdec4328be1a36a5027f274759a564
Sam Heuck <sam@samheuck.com>
2015-03-15 15:04:50 -0500

    converts codemirror text edtior from a view into a component
    Signed-off-by: Sam Heuck <sam@samheuck.com>

---
+++ app/components/text-editor.js
@@ -0,0 +1,39 @@
+/* global CodeMirror */
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    tagName: "textarea",
+
+    didInsertElement: function() {
+        var self = this;
+
+        var el = self.$().get(0);
+        var body = self.get('body');
+
+        var codemirror = new CodeMirror.fromTextArea(el, {
+            mode: 'markdown',
+            lineWrapping: true,
+            keyMap: 'sublime',
+            matchBrackets: true,
+            autoCloseBrackets: true,
+        });
+
+        codemirror.on("change", function (instance) {
+            Ember.run(function () {
+                self.set('body', instance.getValue());
+            });
+        });
+
+        codemirror.setValue(body);
+
+        this.set('editor', codemirror);
+    },
+
+    update: function() {
+        var editor = this.get('editor');
+
+        if (editor.getValue() !== this.get('body')) {
+            editor.setValue(this.get('body'));
+        }
+    }.observes('body')
+});
