71eac34185d6dac59e7a46505c57eb1a22684f84
Sam Heuck <sam@samheuck.com>
2015-03-15 21:44:36 -0500

    converts linkto-attachment helper into a component
    Signed-off-by: Sam Heuck <sam@samheuck.com>

---
+++ app/components/attachment-link.js
@@ -0,0 +1,13 @@
+import config from '../config/environment';
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    tagName: 'a',
+    attributeBindings: ['href', 'target'],
+
+    href: function() {
+        return '%@/%@/%@'.fmt(config.dbHost, config.dbName, this.get('uri'));
+    }.property(),
+
+    target: '_blank',
+});
--- app/helpers/linkto-attachment.js
+++
@@ -1,9 +0,0 @@
-import config from '../config/environment';
-import Ember from 'ember';
-
-export default Ember.Handlebars.makeBoundHelper(function (attachment) {
-    var html = "<a href='%@' target='_blank'>%@</a>",
-        url = "%@/%@/%@".fmt(config.dbHost, config.dbName, attachment.get('id'));
-
-    return new Ember.Handlebars.SafeString(html.fmt(url, attachment.get('file_name')));
-});
