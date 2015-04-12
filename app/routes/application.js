ebdc8b4fd9030fbb23f7a3b86e5cea72b318e33e
Sam Heuck <sam@samheuck.com>
2015-04-11 22:28:56 -0500

    adds preview pane on leaves.index
    also:
    
      - moves New action to the application route/template
      - removes toolbar on leaves.index
    
    Signed-off-by: Sam Heuck <sam@samheuck.com>

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import Notify from 'ember-notify';
 
 export default Ember.Route.extend({
     redirect: function(model, transition) {
@@ -10,6 +11,18 @@ export default Ember.Route.extend({
     actions: {
         back: function() {
             window.history.back();
+        },
+
+        new: function() {
+            var route = this,
+                leaf = this.store.createRecord('leaf');
+
+            leaf.save().then(function () {
+                route.transitionTo('leaf.edit', leaf.get('id'));
+                Notify.success({raw: '<i class="fa"></i> New leaf created!'});
+            }).catch(function () {
+                Notify.warning({raw: '<i class="fa"></i> Something went wrong...'});
+            });
         }
     }
 });
--- app/routes/leaves/index.js
+++ app/routes/leaves/index.js
@@ -1,22 +1,7 @@
 import Ember from 'ember';
-import Notify from 'ember-notify';
 
 export default Ember.Route.extend({
     model: function() {
         return this.store.find('leaf');
-    },
-
-    actions: {
-        new: function() {
-            var route = this,
-                leaf = this.store.createRecord('leaf');
-
-            leaf.save().then(function () {
-                route.transitionTo('leaf.edit', leaf.get('id'));
-                Notify.success({raw: '<i class="fa"></i> New leaf created!'});
-            }).catch(function () {
-                Notify.warning({raw: '<i class="fa"></i> Something went wrong...'});
-            });
-        }
     }
 });
