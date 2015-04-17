f37a068ff7750d0fd296c56b0f771a432289e623
Sam Heuck <sam@samheuck.com>
2015-04-17 00:08:08 -0500

    adds a filter by tag box to leaves nav
    Signed-off-by: Sam Heuck <sam@samheuck.com>

--- app/controllers/leaves/index.js
+++ app/controllers/leaves/index.js
@@ -1,6 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+    sortProperties: ['updated'],
+    sortAscending: false,
+    filterBy: '',
+
+    leaves: function() {
+        var leaves = this.get('arrangedContent');
+        var filter = this.get('filterBy');
+
+        return (filter === '') ? leaves : leaves.filter(hasTag);
+
+        function hasTag(leaf) {
+            return leaf.get('tags').any(function (tag) {
+                return tag.get('tag') === filter;
+            });
+        }
+    }.property('filterBy'),
+
     actions: {
         preview: function (leaf) {
             this.set('previewing', leaf);
